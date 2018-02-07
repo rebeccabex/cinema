cb687b9f4ffc594421af196f0138ab275b46676e
rebeccabex <rebeccabexb@gmail.com>
2018-02-07 10:25:05 +0000

    Test for ticket price

--- cinema/src/main/java/cinema/cinema/Ticket.java
+++ cinema/src/main/java/cinema/cinema/Ticket.java
@@ -2,4 +2,18 @@ package cinema.cinema;
 
 public class Ticket {
 
+	private TicketType type;
+
+	public Ticket(TicketType type) {
+		this.type = type;
+	}
+
+	public TicketType getType() {
+		return type;
+	}
+	
+	public int getTicketPrice() {
+		return 0;
+	}
+	
 }
--- cinema/src/test/java/cinema/cinema/TicketTest.java
+++ cinema/src/test/java/cinema/cinema/TicketTest.java
@@ -1,7 +1,33 @@
 package cinema.cinema;
 
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TicketTest extends TestCase {
 
+	Ticket standard;
+	Ticket oap;
+	Ticket student;
+	Ticket child;
+	
+	@Before
+	public void setup() {
+		standard = new Ticket(TicketType.STANDARD);
+		oap = new Ticket(TicketType.OAP);
+		student = new Ticket(TicketType.STUDENT);
+		child = new Ticket(TicketType.CHILD);
+	}
+	
+	@Test
+	public void testGetPrice() {
+		assertEquals(standard.getTicketPrice(), 8);
+		assertEquals(oap.getTicketPrice(), 6);
+		assertEquals(student.getTicketPrice(), 6);
+		assertEquals(child.getTicketPrice(), 4);
+	}
+	
+	
+	
+	
 }
