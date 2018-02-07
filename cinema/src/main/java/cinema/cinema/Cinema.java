1fab94cea17427b4df30f6b95dd17f1cab55d4ae
Admin <Admin@STUDENT02>
2018-02-07 09:46:12 +0000

    Initial commit

---
+++ cinema/src/main/java/cinema/cinema/Cinema.java
@@ -0,0 +1,13 @@
+package cinema.cinema;
+
+/**
+ * Hello world!
+ *
+ */
+public class Cinema 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ cinema/src/test/java/cinema/cinema/CinemaTest.java
@@ -0,0 +1,38 @@
+package cinema.cinema;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple Cinema.
+ */
+public class CinemaTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CinemaTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( CinemaTest.class );
+    }
+
+    /**
+     * Rigorous Test
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
